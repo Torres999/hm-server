2b71ce4f107d5ff15f468aeb74fcb70ba200ca5e
Neo <neo@email.com>
2025-03-11 12:00:20 +0800

    add test data

--- src/main/java/com/healthmanager/HmServerApplication.java
+++ src/main/java/com/healthmanager/HmServerApplication.java
@@ -3,12 +3,14 @@ package com.healthmanager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.mybatis.spring.annotation.MapperScan;
+import springfox.documentation.oas.annotations.EnableOpenApi;
 
 /**
  * 健康管理小程序后端服务主应用类
  */
 @SpringBootApplication
 @MapperScan("com.healthmanager.mapper")
+@EnableOpenApi
 public class HmServerApplication {
 
     public static void main(String[] args) {
